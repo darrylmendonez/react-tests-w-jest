b1df062df1f8ea08ec5542fad5f69d0d0a10ab71
darrylmendonez <darryl.mendonez@nucleuscentral.com>
2020-02-11 22:14:58 -0500

    added headline component and tests

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import Header from './components/header';
+import Headline from './components/headline';
 import './app.scss';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className="App">
         <Header />
+        <section className="main">
+          <Headline header="Posts" desc="Click the button to render posts" />
+        </section>
       </div>
     )
   }
